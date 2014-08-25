ca2c843a0bad527554a0a420021ea676091db4c5
Maxence G. de Montauzan <maxence@gdemontauzan.fr>
2014-08-25 14:57:33 +0200

    [TMP] Evolution bash : normalisation, correction
    Les fonctions en underscode like
    Doc

--- stopDeamon.sh
+++ stopDeamon.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-sudo docker stop sshd
+# Written by Maxence G. de Montauzan
+
+readonly CONTAINER_NAME="sshd" # Name of the docker container
+readonly IMG_NAME="mgdemontauzan/sshd" # Name of the docker image to run
+
+sudo docker stop $CONTAINER_NAME
 echo "Deamon was stopped"
 
-if [[ -n $(sudo docker ps -a | grep sshd) ]]; then
+if [[ -n $(sudo docker ps -a | grep $CONTAINER_NAME) ]]; then
 	echo ""
 	echo "An old docker container SSHD exists."
 	read -r -p "Would you remove it ? [Y/n] " response
-	if [[ $response =~ (^(yes|y)$|^$) ]]
-	then
+	if [[ $response =~ (^(yes|y)$|^$) ]]; then
 		echo "Removing old sshd container..."
-		sudo docker rm sshd
+		sudo docker rm $CONTAINER_NAME
 	fi
 fi
